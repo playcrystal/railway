8d0a657bc37824f517023010eca87d8963b8c1d8
playcrystal <portalcrystalmc@gmail.com>
2025-01-27 16:41:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createBot () {
 const bot = mineflayer.createBot({
   host: 'hub.playcrystalmc.xyz', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'CrystalHelper', 
+  username: 'CrystalMine', 
   port: 27411, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
